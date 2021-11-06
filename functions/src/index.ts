74cf297255f9e8353ca2a506f9740108ba6f6884
Antonny Santos <antonny.santos@etec.sp.gov.br>
2021-11-06 17:33:29 -0300

    setup :wrench:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,12 @@
+// Config module aliases
 import aliases from 'module-alias';
 aliases.addAlias('~', __dirname);
 
+// Initializing app
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+export * from './api/http/get-users';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
---
+++ functions/src/infra/dependency-injection.ts
@@ -0,0 +1,5 @@
+import { injectable as _injectable, container as _container } from 'tsyringe';
+
+export const injectable = _injectable();
+
+export const container = _container;
